7bff120ea2620abf4622d856c1c2a860ff897a21
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-02-13 16:02:38 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -2,38 +2,43 @@ CREATE DATABASE real_estate_agency;
 
 USE real_estate_agency;
 
-CREATE TABLE client (
-  client_id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  email VARCHAR(100) NOT NULL,
-  password VARCHAR(100) NOT NULL,
-  phone_number VARCHAR(20) NOT NULL
+CREATE TABLE Client(
+   client_id COUNTER,
+   first_name VARCHAR(50) NOT NULL,
+   last_name VARCHAR(50) NOT NULL,
+   email VARCHAR(100) NOT NULL,
+   password VARCHAR(100),
+   phone_number VARCHAR(20),
+   PRIMARY KEY(client_id)
 );
 
-CREATE TABLE ad (
-  ad_id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(100) NOT NULL,
-  price INT NOT NULL,
-  publication_date DATE NOT NULL,
-  last_modification_date DATE NOT NULL,
-  address VARCHAR(200) NOT NULL,
-  category ENUM('sale', 'rent') NOT NULL,
-  type ENUM('apartment', 'house', 'villa', 'office', 'land') NOT NULL,
-  client_id INT,
-  FOREIGN KEY (client_id) REFERENCES client(client_id)
+CREATE TABLE Annonce(
+   ad_id COUNTER,
+   title VARCHAR(100) NOT NULL,
+   price INT NOT NULL,
+   publication_date DATE NOT NULL,
+   last_modification_date DATE NOT NULL,
+   address VARCHAR(200) NOT NULL,
+   category ENUM NOT NULL,
+   type ENUM NOT NULL,
+   client_id INT NOT NULL,
+   PRIMARY KEY(ad_id),
+   FOREIGN KEY(client_id) REFERENCES Client(client_id)
 );
 
-CREATE TABLE ad_images (
-  ad_id INT,
-  image_url VARCHAR(200) NOT NULL,
-  PRIMARY KEY (ad_id, image_url),
-  FOREIGN KEY (ad_id) REFERENCES ad(ad_id)
+CREATE TABLE Image_d_annonce(
+   Id_Image_d_annonce COUNTER,
+   image_url VARCHAR(200),
+   ad_id INT NOT NULL,
+   PRIMARY KEY(Id_Image_d_annonce, image_url),
+   FOREIGN KEY(ad_id) REFERENCES Annonce(ad_id)
 );
 
-CREATE TABLE main_image (
-  ad_id INT,
-  image_url VARCHAR(200) NOT NULL,
-  PRIMARY KEY (ad_id),
-  FOREIGN KEY (ad_id, image_url) REFERENCES ad_images(ad_id, image_url)
+CREATE TABLE image_principale(
+   Id_Image_d_annonce INT,
+   image_url VARCHAR(200),
+   ad_id INT,
+   PRIMARY KEY(Id_Image_d_annonce, image_url, ad_id),
+   FOREIGN KEY(Id_Image_d_annonce, image_url) REFERENCES Image_d_annonce(Id_Image_d_annonce, image_url),
+   FOREIGN KEY(ad_id) REFERENCES Annonce(ad_id)
 );
