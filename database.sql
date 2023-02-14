c8fd36f4ea84c1f18563fc68527734f4431a54a0
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-02-14 16:46:57 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -4,7 +4,7 @@ USE real_estate_agency;
 
 CREATE TABLE
     Client(
-        client_id COUNTER,
+        client_id INT AUTO_INCREMENT,
         first_name VARCHAR(50) NOT NULL,
         last_name VARCHAR(50) NOT NULL,
         email VARCHAR(100) NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE
 
 CREATE TABLE
     Annonce(
-        ad_id COUNTER,
+        ad_id INT AUTO_INCREMENT,
         title VARCHAR(100) NOT NULL,
         price INT NOT NULL,
         publication_date DATE NOT NULL,
@@ -23,8 +23,14 @@ CREATE TABLE
         address VARCHAR(200) NOT NULL,
         City VARCHAR(50) NOT NULL,
         Contry VARCHAR(50) NOT NULL,
-        category ENUM NOT NULL,
-        type ENUM NOT NULL,
+        category ENUM('sale', 'rent') NOT NULL,
+        type ENUM(
+            'apartment',
+            'house',
+            'villa',
+            'office',
+            'land'
+        ) NOT NULL,
         client_id INT NOT NULL,
         PRIMARY KEY(ad_id),
         FOREIGN KEY(client_id) REFERENCES Client(client_id)
@@ -32,9 +38,9 @@ CREATE TABLE
 
 CREATE TABLE
     Image_d_annonce(
-        Id_Image_d_annonce COUNTER,
+        Id_Image_d_annonce INT AUTO_INCREMENT,
         image_url VARCHAR(200),
-        Is_principale LOGICAL NOT NULL,
+        Is_principale BOOLEAN NOT NULL,
         ad_id INT NOT NULL,
         PRIMARY KEY(Id_Image_d_annonce),
         FOREIGN KEY(ad_id) REFERENCES Annonce(ad_id)
